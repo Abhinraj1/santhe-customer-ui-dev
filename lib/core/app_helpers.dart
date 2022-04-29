0a26b879ed1c80ed4385c2f3bd9e016dac543844
Bhavesh Sengunthar <74259681+bhaveshs012@users.noreply.github.com>
2022-04-29 08:44:34 +0530

    copy list feature

--- lib/core/app_helpers.dart
+++ lib/core/app_helpers.dart
@@ -1,28 +1,28 @@
 import 'dart:io';
-
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-class AppHelpers{
-
+class AppHelpers {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  void updateName(String name) => _firebaseAuth.currentUser!.updateDisplayName(name);
+  void updateName(String name) =>
+      _firebaseAuth.currentUser!.updateDisplayName(name);
 
-  void updateEmail(String email) => _firebaseAuth.currentUser!.updateEmail(email);
+  void updateEmail(String email) =>
+      _firebaseAuth.currentUser!.updateEmail(email);
 
   String get getPhoneNumber => _firebaseAuth.currentUser?.phoneNumber ?? '';
 
-  Future<String> get getToken async => await FirebaseMessaging.instance.getToken() ?? '';
+  Future<String> get getToken async =>
+      await FirebaseMessaging.instance.getToken() ?? '';
 
-  Future<String> getDeviceId() async{
+  Future<String> getDeviceId() async {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    if(Platform.isAndroid){
+    if (Platform.isAndroid) {
       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
       return androidInfo.toMap()['androidId'];
-    }
-    else{
+    } else {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
       return iosInfo.toMap()['identifierForVendor'];
     }

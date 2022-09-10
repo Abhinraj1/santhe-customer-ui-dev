8f3565cfa024647fdfb26cf16b0cf96643d2ede9
Neeraj S D <neerajsd04@gmail.com>
2022-09-10 12:44:35 +0530

    added analytics for registration + playstore referrer setup

--- lib/controllers/registrationController.dart
+++ lib/controllers/registrationController.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
+import 'package:android_play_install_referrer/android_play_install_referrer.dart';
 import 'package:get/get.dart';
 
 class RegistrationController extends GetxController {
+  RxString utmMedium = "".obs;
   RxString address = "".obs;
   RxString pinCode = "".obs;
   RxDouble lng = 0.0.obs;
@@ -8,4 +12,18 @@ class RegistrationController extends GetxController {
   RxBool isMapSelected = false.obs;
   RxBool homeDelivery = false.obs;
   RxString howToReach = "".obs;
+
+  Future<void> getReferrerDetails() async {
+    try {
+      ReferrerDetails referrerDetails =
+          await AndroidPlayInstallReferrer.installReferrer;
+      String installReferrer = referrerDetails.installReferrer ?? 'otp';
+      // installReferrer.split('&')[1].split('=')[1];
+      utmMedium.value = installReferrer.split('&')[1].split('=')[1];
+      log('referrerDetails: ${utmMedium.value}');
+    } on Exception catch (e) {
+      utmMedium.value = 'otp';
+      log(e.toString());
+    }
+  }
 }

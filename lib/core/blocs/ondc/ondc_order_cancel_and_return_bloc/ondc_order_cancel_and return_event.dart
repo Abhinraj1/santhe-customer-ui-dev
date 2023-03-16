d557b02b01b22616cb437a9b4708f060e91c02ef
Abhinraj1 <aankith9117@gmail.com>
2023-03-16 17:30:42 +0530

    order details fixes and merged

--- lib/core/blocs/ondc/ondc_order_cancel_and_return_bloc/ondc_order_cancel_and return_event.dart
+++ lib/core/blocs/ondc/ondc_order_cancel_and_return_bloc/ondc_order_cancel_and return_event.dart
@@ -62,10 +62,8 @@ class LoadReasonsForPartialOrderCancelEvent extends ONDCOrderCancelAndReturnEven
     required this.orderNumber,
     required this.previewWidgetModel});
 
-  const LoadReasonsForPartialOrderCancelEvent({required this.orderId,required this.orderNumber});
-
   @override
-  List<Object?> get props => [orderId,orderNumber,orderId];
+  List<Object?> get props => [orderId,orderNumber,previewWidgetModel];
 }
 
 class SelectedCodeEvent extends ONDCOrderCancelAndReturnEvent{

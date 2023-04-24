5cd7a0de62b51fbb27aa87ed124fc476a8d9bdf9
Abhinraj1 <aankith9117@gmail.com>
2023-04-24 19:38:48 +0530

    after merge

--- lib/pages/ondc/ondc_contact_support/ondc_contact_support_enter_query_screen/ondc_contact_support_enter_query_screen_mobile.dart
+++ lib/pages/ondc/ondc_contact_support/ondc_contact_support_enter_query_screen/ondc_contact_support_enter_query_screen_mobile.dart
@@ -24,6 +24,7 @@ class ONDCContactSupportEnterQueryScreenMobile extends StatelessWidget {
 
   final TextEditingController textEditingController = TextEditingController();
 
+
   @override
   Widget build(BuildContext context) {
     String orderId = store.orderNumber.toString();
